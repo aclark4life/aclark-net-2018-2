453b8c252aab98c8745f4c3dfe22049b27b69a51
Alex Clark <aclark@aclark.net>
2015-12-03 12:36:34 -0500

    Update

---
+++ project/app/utils.py
@@ -0,0 +1,11 @@
+def class_name(self):
+    """
+
+    Django Admin object names based on class and pk, e.g.:
+
+    client-1
+    client-2
+    client-3
+    """
+
+    return '-'.join([self.__class__.__name__.lower(), str(self.pk)])
