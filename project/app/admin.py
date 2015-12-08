657b6de365a89a14a28415b1a8c098337218c6a1
Alex Clark <aclark@aclark.net>
2015-12-07 21:10:32 -0500

    Update

--- project/app/admin.py
+++ project/app/admin.py
@@ -29,10 +29,20 @@ class ContactResource(ImportExportModelResource):
     """
 
     class Meta:
+        exclude = ('client', )
         model = Contact
 
     def get_instance(self, instance_loaders, row):
         return False
+
+    def before_import(self, dataset, dry_run, file_name=None, user=None):
+
+        if dataset.headers:
+            dataset.headers = [str(header).lower().strip()
+                               for header in dataset.headers]
+
+        if 'id' not in dataset.headers:
+            dataset.headers.append('id')
 
 
 @admin.register(Contact)
