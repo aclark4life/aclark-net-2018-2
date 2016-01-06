406826ab9a889b0d0d37beb2fff958b442af82de
Alex Clark <aclark@aclark.net>
2016-01-06 18:41:46 -0500

    Update

--- aclarknet/database/tests.py
+++ aclarknet/database/tests.py
@@ -1,3 +1,14 @@
-# from django.test import TestCase
+from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Client
+
+
+class ClientTestCase(TestCase):
+    def setUp(self):
+        Client.objects.create(name="client-1", active=True)
+
+    def test_client_is_active(self):
+        client = Client.objects.get(name="client-1")
+        self.assertEqual(client.active, True)
