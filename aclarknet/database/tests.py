266ca3a4215b4b6379105ec495e362d86c11a86d
Alex Clark <aclark@aclark.net>
2016-01-06 21:55:02 -0500

    Update

--- aclarknet/database/tests.py
+++ aclarknet/database/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 from .models import Client
+from .utils import edit
 
 
 class ClientTestCase(TestCase):
@@ -12,3 +13,14 @@ class ClientTestCase(TestCase):
     def test_client_is_active(self):
         client = Client.objects.get(name="client-1")
         self.assertEqual(client.active, True)
+
+
+class EditTestCase(TestCase):
+    def setUp(self):
+        Client.objects.create(name="client-1", active=True)
+
+    def test_edit(self):
+        """
+        request, form_model, model, url_name, template
+        """
+        client = Client.objects.get(name="client-1")
