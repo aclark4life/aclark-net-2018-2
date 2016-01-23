465f023a34cadf62c5caa71b5de35e0060be8472
Alex Clark <aclark@aclark.net>
2016-01-23 13:53:07 -0500

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -395,7 +395,7 @@ def invoice_edit(request, pk=None):
 @staff_member_required
 def invoice_index(request):
     context = {}
-    fields = ('subject', )
+    fields = ('project__name', 'subject', )
     order_by = '-pk'
     context, items = search(request, Invoice, fields, order_by=order_by)
     context['items'] = items
