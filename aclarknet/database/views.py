07d0adb96d6ba95572c47ae99971bd76ce52d822
Alex Clark <aclark@aclark.net>
2016-01-28 15:45:29 -0500

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -512,13 +512,13 @@ def time(request, pk=None):
 @login_required
 def time_edit(request, pk=None):
 
-    entry = get_object_or_404(Time, pk=pk)
-    if not entry.user and not request.user.is_staff:
-        return HttpResponseRedirect(reverse('admin:index'))
-    if entry.user:
-        if (not entry.user.username == request.user.username and
-                not request.user.is_staff):
-            return HttpResponseRedirect(reverse('admin:index'))
+    #    entry = get_object_or_404(Time, pk=pk)
+    #    if not entry.user and not request.user.is_staff:
+    #        return HttpResponseRedirect(reverse('admin:index'))
+    #    if entry.user:
+    #        if (not entry.user.username == request.user.username and
+    #                not request.user.is_staff):
+    #            return HttpResponseRedirect(reverse('admin:index'))
 
     url_name = 'entry_index'
     kwargs = {}
