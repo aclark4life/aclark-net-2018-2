3fe81bdd3eef433dfa05466aaae9dfdde3fbd7e9
Alex Clark <aclark@aclark.net>
2016-02-18 10:18:50 -0500

    Update

--- aclarknet/database/serializers.py
+++ aclarknet/database/serializers.py
@@ -15,7 +15,7 @@ class ClientSerializer(serializers.HyperlinkedModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'username')
 
 
 class ProfileSerializer(serializers.ModelSerializer):
