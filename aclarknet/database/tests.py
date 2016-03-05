3869564606bf01792e9170e3a605c4bbe1538d45
Alex Clark <aclark@aclark.net>
2016-03-05 18:22:28 -0500

    Update

--- aclarknet/database/tests.py
+++ aclarknet/database/tests.py
@@ -12,24 +12,23 @@ class ClientTestCase(TestCase):
     def setUp(self):
         Client.objects.create(name="client-1", active=True)
 
-    def test_client_is_active(self):
+    def test_is_active(self):
         client = Client.objects.get(name="client-1")
         self.assertEqual(client.active, True)
 
-
-class EditTestCase(TestCase):
-    def setUp(self):
-        Client.objects.create(name="client-1", active=True)
-
-    def test_edit(self):
+    def test_is_editable(self):
         """
         request, form_model, model, url_name, template
         """
         client = Client.objects.get(name="client-1")
+
         user = User(is_staff=True)
         user.save()
         httpclient = HttpClient()
         httpclient.force_login(user)
+
         response = httpclient.post('/client/%s/edit' % client.pk)
+
         self.assertEqual(response.status_code, 302)
-        # edit(response.wsgi_request, ClientForm, Client, 'client', 'client_edit.html')
+
+        edit(response.wsgi_request, ClientForm, Client, 'client_index', 'client_edit.html')
