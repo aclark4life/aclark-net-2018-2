f12fc4e47630488657bdff964a43f3ea2c823e7e
Alex Clark <aclark@aclark.net>
2016-03-06 16:40:42 -0500

    Update

--- aclarknet/database/tests/test_models.py
+++ aclarknet/database/tests/test_models.py
@@ -1,44 +1,22 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from django.test import Client as HttpClient
 from ..models import Contact
-from ..forms import ContactForm
 from ..models import Client
-from ..forms import ClientForm
-from ..utils import edit
 
 # Create your tests here.
 
 
 class ContactTestCase(TestCase):
+
     def setUp(self):
-        Contact.objects.create(active=True, id=1, )
+        Contact.objects.create(active=True, first_name="Alex", last_name="Clark", pk=1)
 
     def test_create(self):
-        contact = Contact.objects.get(id=1)
+        contact = Contact.objects.get(pk=1)
         self.assertEqual(contact.active, True)
+        self.assertEqual(contact.first_name, "Alex")
+        self.assertEqual(contact.last_name, "Clark")
 
-    def test_edit(self):
-        """
-        :params: request, form_model, model, url_name, template
-        """
-        user = User(is_staff=True)
-        user.save()
-
-        httpclient = HttpClient()
-        httpclient.force_login(user)
-
-        contact = Contact.objects.get(id=1)
-        response = httpclient.post('/contact/%s/edit' % contact.pk)
-        self.assertEqual(response.status_code, 302)
-        request = response.wsgi_request
-
-        edit(request,
-             ContactForm,
-             Contact,
-             'contact_index',
-             'contact_edit.html',
-             pk=contact.id)
 
 
 class ClientTestCase(TestCase):
@@ -54,25 +32,3 @@ class ClientTestCase(TestCase):
         self.assertEqual(client.address, "1234 Street")
         self.assertEqual(client.description, "The First Client")
         self.assertEqual(client.name, "Client 1")
-
-    def test_edit(self):
-        """
-        :params: request, form_model, model, url_name, template
-        """
-        user = User(is_staff=True)
-        user.save()
-
-        httpclient = HttpClient()
-        httpclient.force_login(user)
-
-        client = Client.objects.get(name="Client 1")
-        response = httpclient.post('/client/%s/edit' % client.pk)
-        self.assertEqual(response.status_code, 302)
-        request = response.wsgi_request
-
-        edit(request,
-             ClientForm,
-             Client,
-             'client_index',
-             'client_edit.html',
-             pk=client.id)
