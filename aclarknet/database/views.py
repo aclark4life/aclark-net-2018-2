a134099d7ad04d25921b19367f1120d4569d35a0
Alex Clark <aclark@aclark.net>
2016-04-02 19:21:15 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -530,7 +530,7 @@ def report_index(request):
     context = {}
     items = Report.objects.all().annotate(diff=F('gross')-F('net'))
     agg = Report.objects.aggregate(gross=Sum(F('gross')), net=Sum(F('net')))
-    diff = agg.gross - agg.net
+    diff = agg['gross'] = agg['net']
     context['items'] = items
     context['agg'] = agg
     context['diff'] = diff
