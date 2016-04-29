550e434d8b98e04308481040cc98c6395b82cac2
Alex Clark <aclark@aclark.net>
2016-04-29 17:55:47 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -437,7 +437,8 @@ def invoice_index(request):
 def project(request, pk=None):
     context = {}
     project = get_object_or_404(Project, pk=pk)
-    times = Time.objects.filter(project=project).order_by('-date', invoiced=False)
+    times = Time.objects.filter(project=project).order_by('-date',
+                                                          invoiced=False)
     invoices = Invoice.objects.filter(project=project, last_payment_date=None)
     context['project'] = project
     context['times'] = times
