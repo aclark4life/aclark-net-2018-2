ffa1cb08b83c0aa25a995765c23f80459632d4c5
Alex Clark <aclark@aclark.net>
2016-04-29 17:19:41 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -308,7 +308,7 @@ def home(request):
     context = {}
     company = Company.get_solo()
     gross, net = dashboard_totals(Invoice)
-    items = dashboard_items(Project, order_by='client')
+    items = dashboard_items(Project)
     context['company'] = company
     context['gross'] = gross
     context['items'] = items
