b17001ccc0d170014bc1a3cccb4c8264fff3ea98
Alex Clark <aclark@aclark.net>
2016-04-30 17:51:48 -0400

    Update

--- aclarknet/static/aclarknet-database.js
+++ aclarknet/static/aclarknet-database.js
@@ -11,4 +11,10 @@ $(document).ready(function() {
         $('.actives').slideToggle(250);
         return false;
     });
+    function rowStyle(value, row, index) {
+      return {
+        classes: 'text-nowrap another-class',
+        css: {"color": "blue", "font-size": "50px"}
+      };
+    }
 });
