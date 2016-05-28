a515ad3c6988eaaf67bae7bd1cdde483db5d12e6
Alex Clark <aclark@aclark.net>
2016-05-28 10:18:53 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -114,11 +114,11 @@ def generate_doc(request):
     """
     http://stackoverflow.com/a/31904512/185820
     """
+    content_type = 'application/vnd.openxmlformats'
+    content_type += '-officedocument.wordprocessingml.document'
     document = Document()
     document.add_heading('Document Title', 0)
-    response = HttpResponse(
-        content_type=
-        'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    response = HttpResponse(content_type=content_type)
     response['Content-Disposition'] = 'attachment; filename=download.docx'
     document.save(response)
     return response
