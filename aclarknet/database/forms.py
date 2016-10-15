c115ab08f75dc8e69803114a440f6a75d582fe66
Alex Clark <aclark@aclark.net>
2016-10-15 08:22:06 -0400

    Update

--- aclarknet/database/forms.py
+++ aclarknet/database/forms.py
@@ -62,7 +62,7 @@ class MailForm(forms.Form):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('active', 'published', 'bio', 'rate', 'username')
+        fields = ('active', 'published', 'bio', 'rate')
 
 
 class ProjectForm(forms.ModelForm):
