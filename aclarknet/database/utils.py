4bd2043513bc7c37024f8aa8547b771f1f2de27a
Alex Clark <aclark@aclark.net>
2016-10-29 08:46:28 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -95,13 +95,11 @@ def context_items(request,
                   context={},
                   order_by=None,
                   page=None,
-                  paginated=False,
-                  search=''):
+                  paginated=False):
     """
     """
     kwargs = kwargs_for_active_items(model, active=active, user=request.user)
     items = model.objects.filter(Q(**kwargs))
-    items = items_for_search_criteria(fields, items, search)
     if order_by:
         items = items.order_by(order_by)
     if not request.user.is_authenticated:
@@ -391,33 +389,6 @@ def entries_total(queryset):
     total = running_total_co - running_total_dev
     return (entries, running_total_co, running_total_dev, running_total_hours,
             total)
-
-
-def items_for_search_criteria(fields, items, search):
-    filters = []
-    # XXX Logic below flawed; no need to repeat:
-    #
-    #     for field in fields:
-    #         filters.append(Q(**{field + '__icontains': search}))
-    #     items = items.filter(reduce(operator.or_, filters))
-    #
-    if 'date' in fields:
-        expr = re.compile('(\d\d)/(\d\d)/(\d\d\d\d)')
-        if expr.match(search):
-            match = list(expr.match(search).groups())
-            match.reverse()
-            dt = datetime.date(int(match[0]), int(match[2]), int(match[1]))
-            items = items.filter(
-                date__day=dt.day, date__month=dt.month, date__year=dt.year)
-        else:
-            for field in fields:
-                filters.append(Q(**{field + '__icontains': search}))
-            items = items.filter(reduce(operator.or_, filters))
-    else:
-        for field in fields:
-            filters.append(Q(**{field + '__icontains': search}))
-        items = items.filter(reduce(operator.or_, filters))
-    return items
 
 
 def kwargs_for_active_items(model, active=False, user=None):
