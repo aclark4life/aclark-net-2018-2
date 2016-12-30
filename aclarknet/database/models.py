5700b7178d62ae3fb3374b4531731f7c3e5387f9
Alex Clark <aclark@aclark.net>
2016-12-30 18:56:26 -0500

    Update

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -269,6 +269,7 @@ class Service(models.Model):
     company = models.ForeignKey(Company, blank=True, null=True)
     name = models.CharField(max_length=300, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    icon = models.CharField(max_length=25, blank=True, null=True)
 
     def __str__(self):
         return self.name
