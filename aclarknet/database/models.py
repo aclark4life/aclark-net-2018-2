2410cf28297148a6d11a41530da0d3eedf6fc7a8
Alex Clark <aclark@aclark.net>
2017-01-01 09:19:41 -0500

    Update

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -19,6 +19,7 @@ class Client(models.Model):
     address = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     icon = models.CharField(max_length=25, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
