5d2c43eefdf3ad7748312cfdab484eee90d80207
Alex Clark <aclark@aclark.net>
2017-01-17 16:31:42 -0500

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -494,13 +494,13 @@ def paginate(items, page):
 def send_mail(request, subject, message, to):
     recipients = []
     sender = settings.DEFAULT_FROM_EMAIL
-    subject = subject
-    message = message
+#    subject = subject
+#    message = message
     recipients.append(to)
 
     # http://stackoverflow.com/a/28476681/185820
     html_message = render_to_string('cerberus-responsive.html',
-                                    {'username': to})
+                                    {'username': to, 'message': message})
     try:
         django_send_mail(
             subject,
