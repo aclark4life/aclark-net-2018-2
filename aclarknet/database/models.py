c65cf63d5ab4e4041eb7640fd31cfe83059e9dd8
Alex Clark <aclark@aclark.net>
2017-06-11 22:02:22 -0400

    Aha! yapf likes a crlf above class __str__

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -21,6 +21,7 @@ class Client(models.Model):
     description = models.TextField(blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
     url = models.URLField("URL", blank=True, null=True)
+
     def __str__(self):
         return self.name
 
@@ -37,8 +38,10 @@ class Company(SingletonModel):
     currency_symbol = models.CharField(
         "Currency Symbol", default="$", max_length=300, blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return self.name
+
     class Meta:
         verbose_name = u'Company'
 
@@ -90,6 +93,7 @@ class Contact(models.Model):
     # XXX Should this CharField be a UUIDField instead?
     # https://docs.djangoproject.com/en/1.11/ref/models/fields/#uuidfield
     uuid = models.CharField('UUID', max_length=300, default=uuid4)
+
     def __str__(self):
         if self.email:
             return ' '.join(
@@ -128,6 +132,7 @@ class Estimate(models.Model):
         null=True, )
     accepted_date = models.DateField(blank=True, null=True)
     declined_date = models.DateField(blank=True, null=True)
+
     def __str__(self):
         return 'estimate-%s' % self.document_id
 
@@ -178,6 +183,7 @@ class Invoice(models.Model):
     currency_symbol = models.CharField(
         default="$", max_length=300, blank=True, null=True)
     document_type = models.CharField(max_length=300, blank=True, null=True)
+
     def __str__(self):
         return 'invoice-%s' % self.document_id
 
@@ -189,6 +195,7 @@ class Log(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     entry = models.CharField(max_length=300, blank=True, null=True)
+
     def __str__(self):
         return '-'.join([self._meta.verbose_name, str(self.pk)])
 
@@ -205,6 +212,7 @@ class Newsletter(models.Model):
         blank=True,
         related_name="Contacts",
         limit_choices_to={'subscribed': True})
+
     def __str__(self):
         return '-'.join([self._meta.verbose_name, str(self.pk)])
 
@@ -220,6 +228,7 @@ class Note(models.Model):
     updated = models.DateTimeField(auto_now=True)
     requires = models.ManyToManyField(
         'Note', blank=True, related_name="Requires")
+
     def __str__(self):
         return '-'.join([self._meta.verbose_name, str(self.pk)])
 
@@ -244,13 +253,16 @@ class Profile(models.Model):
         decimal_places=2)
     avatar_url = models.URLField(blank=True, null=True)
     notify = models.BooleanField(default=True)
+
     def __str__(self):
         return self.user.username
+
     def get_avatar_url(self):
         if self.avatar_url is not None:
             return self.avatar_url
         else:
             return gravatar_url(self.user.email)
+
     def get_username(self):
         if self.username is not None:
             return self.username
@@ -315,6 +327,7 @@ class Report(models.Model):
     net = models.DecimalField(
         blank=True, null=True, max_digits=12, decimal_places=2)
     notes = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return 'report-%s' % self.date
 
@@ -327,6 +340,7 @@ class Service(models.Model):
     name = models.CharField(max_length=300, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     icon = models.CharField(max_length=25, blank=True, null=True)
+
     def __str__(self):
         return self.name
 
@@ -348,6 +362,7 @@ class Testimonial(models.Model):
     description = models.TextField(blank=True, null=True)
     issue_date = models.DateField(
         "Issue Date", blank=True, null=True, default=timezone.now)
+
     def __str__(self):
         return self.name
 
@@ -368,6 +383,7 @@ class Task(models.Model):
         null=True,
         max_digits=12,
         decimal_places=2)
+
     def __str__(self):
         return self.name
 
@@ -421,8 +437,10 @@ class Time(models.Model):
         Estimate, blank=True, null=True, on_delete=models.SET_NULL)
     invoice = models.ForeignKey(
         Invoice, blank=True, null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return '-'.join([self._meta.verbose_name, str(self.pk)])
+
     # https://docs.djangoproject.com/en/1.9/ref/models/instances/#get-absolute-url
     def get_absolute_url(self, hostname):
         return '%s/%s' % (hostname, reverse('entry', args=[str(self.id)]))
