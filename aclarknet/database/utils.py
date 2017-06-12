f9a4289277c58ec77d911c94c58f01b3be56605f
Alex Clark <aclark@aclark.net>
2017-06-12 18:40:36 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -506,6 +506,8 @@ def obj_copy(obj, url_name):
         url_name = 'entry_edit'
     elif obj._meta.verbose_name == 'newsletter':
         url_name = 'newsletter_edit'
+    elif obj._meta.verbose_name == 'contract':
+        url_name = 'contract_edit'
     return HttpResponseRedirect(reverse(url_name, kwargs=kwargs))
 
 
