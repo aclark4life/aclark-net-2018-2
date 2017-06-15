eee16439f466c5be2f51bc162062465eb86e27f2
Alex Clark <aclark@aclark.net>
2017-06-15 19:47:58 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -283,11 +283,17 @@ def contract(request, pk=None):
     # could clean this up.
     estimate = contract.statement_of_work
     if estimate:
-        times = Time.objects.filter(estimate=estimate)
+        times_client = Time.objects.filter(
+            client=estimate.client,
+            estimate=None,
+            project=None,
+            invoiced=False,
+            invoice=None)
+        times_estimate = Time.objects.filter(estimate=estimate)
+        times = times_client | times_estimate
         times = times.order_by('-date')
     else:
         times = None
-    context['times'] = times
     if pdf:
         response = HttpResponse(content_type='application/pdf')
         filename = get_filename(company)
