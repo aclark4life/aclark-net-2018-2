7b39074fe2aaaab65e9a339b2a4b89b76ad43135
Alex Clark <aclark@aclark.net>
2017-06-21 16:41:41 -0400

    Update

--- aclarknet/static/app.js
+++ aclarknet/static/app.js
@@ -1,3 +1,18 @@
 import bar from './bar';
 
 bar();
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var Hello = React.createClass ({
+    render: function() {
+        return (
+            <h1>
+            Hello, React!
+            </h1>
+        )
+    }
+})
+
+ReactDOM.render(<Hello />, document.getElementById('container'))
