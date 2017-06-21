257ddca61302d83381c9087c04a01786a64a652e
Alex Clark <aclark@aclark.net>
2017-06-21 16:55:46 -0400

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -174,13 +174,4 @@ REST_FRAMEWORK = {
 
 # http://geezhawk.github.io/using-react-with-django-rest-framework
 # https://github.com/ezhome/django-webpack-loader
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'CACHE': not DEBUG,
-        'BUNDLE_DIR_NAME': 'webpack_bundles/', # must end with slash
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
-        'POLL_INTERVAL': 0.1,
-        'TIMEOUT': None,
-        'IGNORE': ['.+\.hot-update.js', '.+\.map']
-    }
-}
+WEBPACK_LOADER = {}
