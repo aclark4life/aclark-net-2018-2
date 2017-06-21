dbd074260ec7d63f01d88bc5de3ffca481719467
Alex Clark <aclark@aclark.net>
2017-06-21 16:30:28 -0400

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+// http://geezhawk.github.io/using-react-with-django-rest-framework
+// https://webpack.js.org
+
+//require our dependencies
+
+var path = require('path')
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
 module.exports = {
   entry: './aclarknet/static/app.js',
   output: {
