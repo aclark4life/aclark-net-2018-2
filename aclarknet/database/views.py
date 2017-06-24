7422f2f75f90ead491e317bbc7b894525b439b18
Alex Clark <aclark@aclark.net>
2017-06-24 08:18:24 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -1132,6 +1132,7 @@ def user_edit(request, pk=None):
     kwargs = {}
     user = get_object_or_404(User, pk=pk)
     profile = Profile.objects.get_or_create(user=user)[0]
+    profile.save()
     context['user'] = user
     url_name = 'user_index'
     if pk:
