2eb45b35fcb40fc5b3e647989d11813404fcf93d
Alex Clark <aclark@aclark.net>
2017-06-25 16:06:27 -0400

    Update

--- aclarknet/database/forms.py
+++ aclarknet/database/forms.py
@@ -168,7 +168,7 @@ class NoteForm(forms.ModelForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('active', 'published', 'bio', 'rate', 'icon_size', 'page_size', 'dashboard_choices')
+        fields = ('active', 'published', 'bio', 'rate', 'icon_size', 'page_size', 'override_dashboard', 'dashboard_choices')
 
 
 class ProjectForm(forms.ModelForm):
