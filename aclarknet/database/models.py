fedda7ce048ff4f2212fdfef995e5f0e18235d49
Alex Clark <aclark@aclark.net>
2017-07-02 09:06:00 -0400

    Update

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -271,6 +271,7 @@ class Note(models.Model):
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     due_date = models.DateField(blank=True, null=True)
+    hidden = models.BooleanField(default=False)
     note = models.TextField(blank=True, null=True)
     priority = models.IntegerField(blank=True, null=True)
     updated = models.DateTimeField(auto_now=True)
