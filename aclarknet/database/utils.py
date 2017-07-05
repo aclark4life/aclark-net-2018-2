539b529fa139a64c74f5ce1dbb3925547cf074fa
Alex Clark <aclark@aclark.net>
2017-07-05 09:07:33 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -692,7 +692,7 @@ def obj_edit(obj,
         for field in contract_settings._meta.fields:
             if field.description == 'Text' and field.name != 'body':
                 text = ''.join([text, '<h2>', field.verbose_name, '</h2>'])
-                text = ''.join([text, '<p>', field.get_default(), '</p>'])
+                text = ''.join([text, '<p>', getattr(obj, field.name), '</p>'])
         setattr(obj, 'body', text)
         obj.save()
     if obj._meta.verbose_name == 'note' and company_note:
