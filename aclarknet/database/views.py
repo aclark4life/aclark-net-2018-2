36c7f5af707e99ebfe24dc02d0491fcdc24a838b
Alex Clark <aclark@aclark.net>
2017-07-05 19:18:04 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -495,9 +495,9 @@ def estimate_mail(request, pk=None):
     end_date = estimate.project.end_date
     for entry in estimate.time_set.all():
         if counter != 0:
-            notes += '</li><li>'.join(entry.notes)
+            notes += '</li><li>%s' % entry.notes
         else:
-            notes += ''.join(entry.notes)
+            notes += entry.notes
         counter += 1
         hours += entry.hours
     notes += '</li></ul>'
