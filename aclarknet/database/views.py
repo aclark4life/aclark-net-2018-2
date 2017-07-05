7a492a2b35bb92c00ab111448a2e8b07f5a5b71c
Alex Clark <aclark@aclark.net>
2017-07-05 07:17:39 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -1273,4 +1273,13 @@ def user_index(request):
         order_by=('-profile__active', ),
         show_search=False)
     context['company'] = company
+    # Check if user is contact
+    contacts = Contact.objects.all()
+    items = context['items']
+    for item in items:
+        if item.email in [i.email for i in contacts]:
+            item.is_contact = True
+        else:
+            item.is_contact = False
+    context['items'] = items
     return render(request, 'user_index.html', context)
