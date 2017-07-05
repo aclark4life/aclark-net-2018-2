6c112cfa37bc35da11c11732008dbda6749d8707
Alex Clark <aclark@aclark.net>
2017-07-05 09:40:51 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -489,8 +489,7 @@ def estimate_mail(request, pk=None):
             request,
             'Estimate',
             'Here is the estimate',
-            django_settings.EMAIL_FROM,
-            url=url):
+            django_settings.EMAIL_FROM)
         messages.add_message(request, messages.SUCCESS, 'Mail sent!')
         log = Log(entry='Estimate sent to %s.' % to)
         log.save()
