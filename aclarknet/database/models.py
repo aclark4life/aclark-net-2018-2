d4f58c8526423e3b21dee929ffe0f4b696dd9350
Alex Clark <aclark@aclark.net>
2017-07-06 06:50:45 -0400

    Update

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -34,8 +34,8 @@ class Client(models.Model):
     icon = models.CharField(max_length=25, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    url = models.URLField("Website", blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
-    url = models.URLField("URL", blank=True, null=True)
 
     def __str__(self):
         return self.name
