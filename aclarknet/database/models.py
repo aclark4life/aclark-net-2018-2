c25bc39f140b442001bf06fc392d62fc897f4e47
Alex Clark <aclark@aclark.net>
2017-07-10 17:37:52 -0400

    Update

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -466,7 +466,7 @@ class Service(models.Model):
     company = models.ForeignKey(Company, blank=True, null=True)
     name = models.CharField(max_length=300, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    icon = models.CharField(max_length=25, blank=True, null=True)
+    icon = models.CharField("Font Awesome Icon (without 'fa-')", max_length=25, blank=True, null=True)
 
     def __str__(self):
         return self.name
