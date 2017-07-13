f0344f016df3d462dbbb9428e17d3b32d5e2b9d7
Alex Clark <aclark@aclark.net>
2017-07-12 20:23:48 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -809,8 +809,8 @@ def project(request, pk=None):
 
 @staff_member_required
 def project_edit(request, pk=None):
-    # client = request.GET.get('client')
-    # client = get_object_or_404(Client, pk=client)
+    client = request.GET.get('client')
+    client = get_object_or_404(Client, pk=client)
     # clients = Client.objects.filter(active=True)
     kwargs, url_name = get_url_name(
         'project', page_type='index_or_edit', pk=pk)
@@ -821,7 +821,7 @@ def project_edit(request, pk=None):
         url_name,
         'project_edit.html',
         active_nav='project',
-        # client=client,
+        client=client,
         # clients=clients,
         kwargs=kwargs,
         pk=pk)
