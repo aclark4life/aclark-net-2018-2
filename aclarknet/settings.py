78d3b2730f29760082440bda3dbc5b7e6d21cb37
Alex Clark <aclark@aclark.net>
2017-07-14 09:33:51 -0400

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'crispy_forms',
     'django_extensions',
     'import_export',
