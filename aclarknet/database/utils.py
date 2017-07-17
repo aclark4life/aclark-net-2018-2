abadedee8afbe21df076c4f65fd31ca674677779
Alex Clark <aclark@aclark.net>
2017-07-16 20:35:07 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -439,7 +439,7 @@ def get_entries(queryset):
     for entry in queryset:
         line_total = get_line_total(entries, entry)
         entries[entry] = {}
-        entries[entry]['amount'] = line_total
+        entries[entry]['amount'] = '%.2f' % line_total
         entries[entry]['date'] = entry.date
         entries[entry]['hours'] = entry.hours
         entries[entry]['line_total'] = '%.2f' % line_total
