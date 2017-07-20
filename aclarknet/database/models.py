0b2fbe2467ee4ac9d8839e1441c4973a212ac968
Alex Clark <aclark@aclark.net>
2017-07-20 06:52:59 -0400

    Update

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -329,7 +329,7 @@ class Note(models.Model):
     due_date = models.DateField("Due", blank=True, null=True)
     title = models.CharField(max_length=300, blank=True, null=True)
     note = models.TextField(blank=True, null=True)
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         if self.title:
