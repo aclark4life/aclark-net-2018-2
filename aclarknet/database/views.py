d9d72fc6d73b88742253613d06da84a1e820e2cb
Alex Clark <aclark@aclark.net>
2017-07-25 20:34:03 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -633,7 +633,6 @@ def note_edit(request, pk=None):
 
 @staff_member_required
 def note_index(request, pk=None):
-    filters = {'hidden': False, }
     context = get_index_items(
         request,
         Note,
