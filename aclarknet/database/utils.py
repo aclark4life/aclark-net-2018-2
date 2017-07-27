d4eabab5336e4cc3af98822763c172deb80ecba9
Alex Clark <aclark@aclark.net>
2017-07-27 06:48:16 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -922,6 +922,7 @@ def set_relationship(obj,
             obj.save()
         elif query_project:
             project = get_object_or_404(project_model, pk=query_project)
+            obj.project = project
             obj.task = project.task
             obj.save()
         return True
