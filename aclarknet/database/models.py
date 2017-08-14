595fc9cdefe59b9dcf8a9540d38d32350f748b79
Alex Clark <aclark@aclark.net>
2017-08-14 16:50:42 -0400

    Update

--- aclarknet/database/models.py
+++ aclarknet/database/models.py
@@ -378,7 +378,7 @@ class Note(BaseModel):
     title = models.CharField(max_length=300, blank=True, null=True)
     tags = TaggableManager(blank=True, help_text='')
     note = models.TextField(blank=True, null=True)
-    contacts = models.ManyToManyField('Contact', blank=True)
+    contacts = models.ManyToManyField('Contact', blank=True, limit_choices_to={'active': True})
 
     def __str__(self):
         if self.title:
