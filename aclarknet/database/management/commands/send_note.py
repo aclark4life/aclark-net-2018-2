3db27470ab61a48f45938e0088d7c61cd2585171
Alex Clark <aclark@aclark.net>
2017-08-15 16:20:49 -0400

    Update

--- aclarknet/database/management/commands/send_note.py
+++ aclarknet/database/management/commands/send_note.py
@@ -14,7 +14,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         note_id = options.get('note_id')[0]
         obj = Note.objects.get(pk=note_id)
-        if mail_send(**mail_compose(obj)):
+        recipients, status = mail_send(**mail_compose(obj))
+        if status:
             self.stdout.write(self.style.SUCCESS('Mail sent!'))
         else:
             self.stdout.write(self.style.SUCCESS('Mail not sent!'))
