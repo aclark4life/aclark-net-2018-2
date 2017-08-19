6eb73f079676e3756c9c0a0d5868c8420f258af0
Alex Clark <aclark@aclark.net>
2017-08-19 10:51:45 -0400

    Update

--- aclarknet/database/forms.py
+++ aclarknet/database/forms.py
@@ -168,7 +168,7 @@ class MailForm(forms.Form):
     send_html = forms.BooleanField(label='Send HTML message', required=False)
     template = forms.ChoiceField(required=False, choices=TEMPLATE_CHOICES)
     subject = forms.CharField(required=False)
-    message = forms.CharField(widget=forms.Textarea(), required=False)
+    message = forms.CharField(widget=forms.widgets.TextInput(attrs={'class': 'tinymce'}), required=False)
 
 
 class NewsletterForm(forms.ModelForm):
