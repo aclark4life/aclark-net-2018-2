e1333b313ecf0c6d336cc8303efc31440a8984c5
Alex Clark <aclark@aclark.net>
2017-08-21 06:48:30 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -723,18 +723,19 @@ def task_index(request):
 @login_required
 def time(request, pk=None):
     """
-    Users can only see their own time entries unless staff.
+    Authenticate users can only see their own time entries unless
+    they are staff members.
     """
-    message = 'Not allowed.'
+    message = 'Sorry, you are not allowed to see that.'
     time_entry = get_object_or_404(Time, pk=pk)
     # No user
     if not time_entry.user and not request.user.is_staff:
-        messages.add_message(request, messages.WARNING, message)
+        messages.add_message(request, messages.DANGER, message)
         return HttpResponseRedirect(reverse('home'))
     # Time entry user does not much request user
     elif (not time_entry.user.username == request.user.username and
           not request.user.is_staff):
-        messages.add_message(request, messages.WARNING, message)
+        messages.add_message(request, messages.DANGER, message)
         return HttpResponseRedirect(reverse('home'))
     else:
         context = get_page_items(
