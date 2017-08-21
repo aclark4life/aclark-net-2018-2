d87320ed882bcc4d36539c3211fdb641c9f80149
Alex Clark <aclark@aclark.net>
2017-08-21 08:05:04 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -808,7 +808,7 @@ def time_index(request):
 
 @login_required
 def user(request, pk=None):
-    if not request.user.pk == pk:
+    if not request.user.pk == int(pk):
         message = 'Sorry, you are not allowed to view that user.'
         messages.add_message(request, messages.WARNING, message)
         return HttpResponseRedirect(reverse('home'))
