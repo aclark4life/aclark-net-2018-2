5f816eafa1bb241947faee12c4cd9f56f72ce501
Alex Clark <aclark@aclark.net>
2017-08-21 21:04:26 -0400

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -193,7 +193,7 @@ GEOIP_PATH = os.path.join(BASE_DIR, '..', 'data')
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Errors sent to admins
-SERVER_EMAIL = 'db.aclark.net'
+SERVER_EMAIL = 'aclark@aclark.net'
 ADMINS = [('Alex Clark', 'aclark@aclark.net'), ]
 
 # AWS SES
