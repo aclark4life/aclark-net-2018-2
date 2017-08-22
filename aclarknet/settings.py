55c00476cfd7cfcf6e4e36aeb9dbb79a73b990bd
Alex Clark <aclark@aclark.net>
2017-08-21 20:00:45 -0400

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -200,4 +200,4 @@ EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = os.environ.get('SMTP_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASS')
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
