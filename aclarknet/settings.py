6d57fc3336477b197c777fc4f94c142d5a55cb04
Alex Clark <aclark@aclark.net>
2017-08-25 05:33:53 -0400

    Update

--- aclarknet/settings.py
+++ aclarknet/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 # Whitenoise
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
