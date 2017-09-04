4636dd296413a9db35ac152884469c336635c789
Alex Clark <aclark@aclark.net>
2017-09-04 14:29:20 -0400

    Update

--- aclarknet/database/views.py
+++ aclarknet/database/views.py
@@ -124,7 +124,7 @@ def client_index(request):
     context = get_index_items(
         app_settings_model=SettingsApp,
         model=Client,
-        order_by=('-updated', '-active'),
+        order_by=('-active', '-updated'),
         request=request,
         search_fields=('address', 'name'),
         show_search=True)
