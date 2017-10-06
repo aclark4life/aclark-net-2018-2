92b5504dc795345fd405710d0ccb73ab22588811
Alex Clark <aclark@aclark.net>
2017-10-06 19:54:19 -0400

    Update

--- aclarknet/rock_n_roll/__init__.py
+++
@@ -1,3 +0,0 @@
-# rock_n_roll/__init__.py
-
-default_app_config = 'aclarknet.rock_n_roll.apps.RockNRollConfig'
--- aclarknet/rock_n_roll/apps.py
+++
@@ -1,8 +0,0 @@
-# rock_n_roll/apps.py
-
-from django.apps import AppConfig
-
-
-class RockNRollConfig(AppConfig):
-    name = 'aclarknet.rock_n_roll'
-    verbose_name = "Rock ’n’ roll"
