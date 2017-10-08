508863431cc470f5e72a59da45b619060a6f70bf
Alex Clark <aclark@aclark.net>
2017-10-08 14:01:10 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -164,8 +164,8 @@ def edit(request, **kwargs):
                     project_model=project_model)
                 if model_name == 'time':
                     status_message = {
-                        'success': 'Time entry created.',
-                        'failure': 'Time entry not created.',
+                        'success': 'Time entry created',
+                        'failure': 'Time entry not created',
                     }
                     mail_process(
                         obj,
