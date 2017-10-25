b44c1b8d07ec351eb78d53d4d41447d4ce556646
Alex Clark <aclark@aclark.net>
2017-10-25 17:04:16 -0400

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -636,7 +636,7 @@ def get_page_items(**kwargs):
                 context['dashboard_items'] = dashboard_items
                 # Items
                 estimates = estimate_model.objects.filter(
-                    accepted_date=None, is_to=False, is_sow=False)
+                    accepted_date=None, hidden=False, is_to=False, is_sow=False)
                 invoices = invoice_model.objects.filter(last_payment_date=None)
                 invoices = invoices.order_by(*order_by['invoice'])
                 notes = note_model.objects.filter(active=True, hidden=False)
