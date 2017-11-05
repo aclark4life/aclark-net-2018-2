5774eab94a3b3895cd131e767f10a81c00f5e74b
Alex Clark <aclark@aclark.net>
2017-11-05 07:11:02 -0500

    Update

--- aclarknet/database/utils.py
+++ aclarknet/database/utils.py
@@ -33,7 +33,6 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from operator import or_ as OR
 import decimal
-import numpy as np
 
 fake = Faker()
 geo_ip = GeoIP2()
@@ -700,25 +699,32 @@ def get_page_items(**kwargs):
 def get_plot(request):  # http://stackoverflow.com/a/5515994/185820
     """
     """
+    costs = get_query(request, 'costs')
     grosses = get_query(request, 'grosses')
     nets = get_query(request, 'nets')
-    # http://matplotlib.org/examples/api/date_demo.html
-    x1 = [
+
+    # Cost
+    x1 = [  # http://matplotlib.org/examples/api/date_demo.html
+        date2num(timezone.datetime.strptime(i[1], '%Y-%m-%d')) for i in costs
+    ]
+    y1 = [i[0] for i in costs]
+    # Gross
+    x2 = [
         date2num(timezone.datetime.strptime(i[1], '%Y-%m-%d')) for i in grosses
     ]
-    y1 = [i[0] for i in grosses]
-    x2 = [date2num(timezone.datetime.strptime(i[1], '%Y-%m-%d')) for i in nets]
-    y2 = [i[0] for i in nets]
-    # x1 = np.array(x1)
-    # y1 = np.array(y1)
-    # x2 = np.array(x2)
-    # y2 = np.array(y2)
+    y2 = [i[0] for i in grosses]
+
+    # Net
+    x3 = [date2num(timezone.datetime.strptime(i[1], '%Y-%m-%d')) for i in nets]
+    y3 = [i[0] for i in nets]
+
     figure = Figure()
     canvas = FigureCanvasAgg(figure)
     axes = figure.add_subplot(1, 1, 1)
     axes.grid(True)
     axes.plot(x1, y1)
     axes.plot(x2, y2)
+    axes.plot(x3, y3)
     axes.xaxis.set_major_formatter(DateFormatter('%m'))
     # write image data to a string buffer and get the PNG image bytes
     buf = BytesIO()
