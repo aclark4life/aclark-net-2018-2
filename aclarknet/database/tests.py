3e21427c9314854926e96a78ec15d48596e50cdf
Alex Clark <aclark@aclark.net>
2017-11-06 06:28:44 -0500

    Update

--- aclarknet/database/tests.py
+++ aclarknet/database/tests.py
@@ -2,11 +2,12 @@ from django.test import TestCase
 from .models import Time
 
 
-class AnimalTestCase(TestCase):
+class TimeEntryTestCase(TestCase):
     def setUp(self):
         Time.objects.create(log="Competently innovate corporate innovation")
 
-    def test_animals_can_speak(self):
-        """Animals that can speak are correctly identified"""
+    def test_time_entry_log(self):
+        """
+        """
         time = Time.objects.get(pk=1)
         self.assertEqual(time.log, "Competently innovate corporate innovation")
