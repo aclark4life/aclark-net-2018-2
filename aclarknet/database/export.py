e3355b0b7e6549b19974b84a70758a3c78f4818c
Alex Clark <aclark@aclark.net>
2017-11-15 08:43:44 -0500

    Update

--- aclarknet/database/export.py
+++ aclarknet/database/export.py
@@ -6,13 +6,6 @@ from django_xhtml2pdf.utils import generate_pdf
 
 def render_pdf(request, template, context, pk=None):
     response = HttpResponse(content_type='application/pdf')
-    company_name = context['company_name']
-    model_name = context['model_name']
-    model_name = model_name.upper()
-    if company_name:
-        filename = '_'.join([company_name, model_name, pk])
-    else:
-        filename = '_'.join(['Company', model_name, pk])
-    response['Content-Disposition'] = 'filename=%s.pdf' % filename
+    response['Content-Disposition'] = 'filename=test.pdf'
     return generate_pdf(
         'pdf_invoice.html', context=context, file_object=response)
