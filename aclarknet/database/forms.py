9d93c1e3a5b475bc699d851c5b0dca6c9e92e4da
Alex Clark <aclark@aclark.net>
2017-11-28 08:03:39 -0500

    Update

--- aclarknet/database/forms.py
+++ aclarknet/database/forms.py
@@ -27,7 +27,7 @@ class AdminProfileForm(forms.ModelForm):
         model = Profile
         fields = ('rate', 'preferred_payment_method', 'bio', 'address',
                   'dashboard_override', 'dashboard_choices', 'icon_size',
-                  'published')
+                  'notify', 'published')
         widgets = {
             'bio': forms.widgets.TextInput(attrs={
                 'class': 'tinymce'
